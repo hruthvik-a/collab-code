11b3f05fac7d55b773bd8af9338d6b532ffd61e4
hruthvik.a <83105008+hruthvik-a@users.noreply.github.com>
2023-11-07 11:24:55 +0530

    Update API_EndPoints.ts

--- client/src/APIs/API_EndPoints.ts
+++ client/src/APIs/API_EndPoints.ts
@@ -1,4 +1,4 @@
-const server = "https://collab-code-57t1.vercel.app/login";
+const server = "https://collab-code-57t1.vercel.app";
 
 export const login = server + "/login";
 export const register = server + "/register";
